2620929e8d25845b6e1b1b170248847844498762
Teusocost <mateus.costaar@gmail.com>
2023-10-17 20:28:05 +0100

    new paste

---
+++ alterar_clock/alterarclock.ino
@@ -0,0 +1,32 @@
+#include <Arduino.h>
+uint32_t current_freq;
+void setup() {
+  Serial.begin(115200);
+  delay(1000); // Aguarde um momento para estabilização da comunicação serial
+  Serial.println("Iniciando com a frequência padrão da CPU.");
+}
+
+void loop() {
+  // Obtenha e imprima a frequência atual da CPU em MHz
+  current_freq = getCpuFrequencyMhz();
+  Serial.print("Frequencia da CPU atual: ");
+  Serial.print(current_freq);
+  Serial.println(" MHz");
+
+  // Altere a frequência da CPU para 80 MHz
+  setCpuFrequencyMhz(80);
+  Serial.println("Alterando a frequência da CPU para 80 MHz.");
+
+  current_freq = getCpuFrequencyMhz();
+  Serial.print("Frequencia da CPU atual: ");
+  Serial.print(current_freq);
+  Serial.println(" MHz");
+
+  delay(5000); // Aguarde 5 segundos
+
+  // Restaure a frequência da CPU para 240 MHz
+  setCpuFrequencyMhz(240);
+  Serial.println("Restaurando a frequência da CPU para 240 MHz.");
+
+  delay(5000); // Aguarde mais 5 segundos
+}
